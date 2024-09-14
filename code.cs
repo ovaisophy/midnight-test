6af03567eb356cb47b5e862be0cda8e1c688ff53
ovaisophy <68985203+ovaisophy@users.noreply.github.com>
2024-09-14 22:53:08 +0300

    added variable num5

--- code.cs
+++ code.cs
@@ -3,17 +3,18 @@ public class A
 {
     public static void Main()
     {
-        int num1, num2, num3, num4;
+        int num1, num2, num3, num4, num5;
         num1 = 5;
         num2 = 3;
         num3 = A.strange(num1, num2);
         num4 = A.strange(num3, num1);
+        num5 = A.strange(num2, num3);
         string str = "num3= " + num3 + " and num4= " + num4;
         Console.Write(str);
     }
 
     public static int strange(int x, int y)
     {
-        return y - x;
+        return y + x;
     }
 }
